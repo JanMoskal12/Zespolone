cf6338ec4b66854a1e7469b1245f4823ffde0fb3
JanMoskal12 <janmoskal03@gmail.com>
2023-06-11 19:36:01 +0200

    oooo

--- src/zespolona.cpp
+++ src/zespolona.cpp
@@ -19,6 +19,13 @@ double zespolona :: y() const{
 }
 
 ostream &operator<<(ostream &os, zespolona z){
+    if(z.x() == 0 && z.y() == 0)
+    os << 0 << '\n';
+    else if(z.x() == 0)
+    os << z.y() << "i" << '\n';
+    else if(z.y() == 0)
+    os << z.x() << '\n';
+    else
     os << z.x() << "+" << z.y() << "i" << '\n';
     return os;
 }
